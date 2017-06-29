2be3137e91e0e4355e2bac7950ad669be1c29bdc
闻飞 <780389404@qq.com>
2017-06-29 23:03:50 +0800

    提交 整体的布局

--- learnSwift/ViewController.swift
+++ learnSwift/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController:UIViewController
     
     var tableView  : UITableView?
     var item : NSArray?
+    var lable : UILabel?
     
     
     override func viewDidLoad() {
@@ -21,67 +22,49 @@ class ViewController:UIViewController
         print("swift")
         
         
-        
-        
-        
-        
-        
-        
-        
+//        
 //        self.item=["1", "2", "3", "4", "5"]
 //        self.tableView = UITableView(frame:self.view.frame,style:UITableViewStyle.plain)
 //        self.tableView!.delegate=self;
 //        self.tableView!.dataSource=self;
 //        self.tableView!.register(UITableViewCell.self, forCellReuseIdentifier:"cell")
 //        self.view .addSubview(self.tableView!)
+//        
         
         
+        let box = UIView()
+        box.backgroundColor=UIColor.yellow
+        box.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(box)
+        box.snp_makeConstraints { (make) -> Void in
+            //注释：box距离父视图上下左右边距都是20像素
+            make.edges.equalTo(self.view).inset(UIEdgeInsetsMake(20, 20, 20, 20))
+        }
         
+        let button = UIButton()
         
+        button.backgroundColor = UIColor.red
         
-//        let box = UIView()
-//        box.backgroundColor=UIColor.yellow
-//        box.translatesAutoresizingMaskIntoConstraints = false
-//        self.view.addSubview(box)
-//        box.snp_makeConstraints { (make) -> Void in
-//            //注释：box距离父视图上下左右边距都是20像素
-//            make.edges.equalTo(self.view).inset(UIEdgeInsetsMake(20, 20, 20, 20))
-//        }
-//        
-//        let button = UIButton()
-//        
-//        button.backgroundColor = UIColor.red
-//        
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        box.addSubview(button)
-//        
-//        button.snp_makeConstraints { (make) -> Void in
-//            //注释：box距离父视图上下左右边距都是20像素
-//            
-//           make.left.equalTo(20)
-//            
-//           make.right.equalTo(-20)
-//            
-//           make.top.equalTo(20)
-//            
-//            
-//          make.bottom.equalTo(-20)
-//            
-//
-//        }
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        box.addSubview(button)
+        
+        button.snp_makeConstraints { (make) -> Void in
+            //注释：box距离父视图上下左右边距都是20像素
+            
+           make.left.equalTo(20)
+            
+           make.right.equalTo(-20)
+            
+           make.top.equalTo(20)
+            
+            
+          make.bottom.equalTo(-20)
+            
+
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
